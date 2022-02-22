e45b7cc343a2096ae33bd65f8ea223cdcd68d61a
Marcus Liu <me@mail.lmarcus.onl>
2022-02-23 01:17:12 +0800

    添加後續修改 package.json
    後續應以 jq 的方式處理

--- express/init.sh
+++ express/init.sh
@@ -4,3 +4,10 @@ npm install express
 npm install express-handlebars
 cp ../swissknife/express/app.js . 
 cp ../swissknife/express/views/ .
+
+echo "package.json 加入常用指令"
+echo "main app.js \n   
+    scripts: { \n
+    start: node app.js, \n
+    dev: nodemon app.js 
+    "
